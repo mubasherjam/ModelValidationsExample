17ce2f1ef6aabbe1dcb313324f27f6fd8b4c9a6e
Mubasher Jam <mubasherworld@gmail.com>
2026-08-26 14:51:34 +0500

    Add project files.

---
+++ ModelValidationsExample/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelValidationsExample.Models;
+
+namespace ModelValidationsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [Route("register")]
+        public IActionResult Index(Person person)
+        {
+            return Content($"{person}");
+        }
+    }
+}
---
+++ ModelValidationsExample/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelValidationsExample.Models
+{
+    public class Person
+    {
+        [Required(ErrorMessage = "Person name is required.")]
+        public string? PersonName { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
+        public double? Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"PersonName: {PersonName}, Email: {Email}, Phone: {Phone}," +
+                $"Password: {Password}, ConfirmPassword: {ConfirmPassword}," +
+                $"Price: {Price}";
+        }
+    }
+}
