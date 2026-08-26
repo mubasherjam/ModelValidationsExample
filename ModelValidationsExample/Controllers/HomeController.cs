517542cdd85be5e1f8ef396d3d3fe8c8aebc6ed8
Mubasher Jam <mubasherworld@gmail.com>
2026-08-26 15:33:56 +0500

    Simplify error handling in HomeController
    Refactored the `Index` action method to streamline the process of extracting and concatenating error messages from `ModelState`. Replaced nested loops with a LINQ query for improved readability and reduced verbosity. Updated the `BadRequest()` call to include the concatenated error messages directly.

--- ModelValidationsExample/Controllers/HomeController.cs
+++ ModelValidationsExample/Controllers/HomeController.cs
@@ -11,16 +11,11 @@ namespace ModelValidationsExample.Controllers
         {
             if (!ModelState.IsValid)
             {
-                List<string> errorsList = new List<string>(); 
-                foreach (var value in ModelState.Values)
-                {
-                    foreach (var error in value.Errors)
-                    {
-                        errorsList.Add(error.ErrorMessage);
-                    }
-                }
-               string errors = string.Join ("\n", errorsList);
-                return BadRequest ();
+                string errors = string.Join("\n",
+                    ModelState.Values.SelectMany(v => v.Errors)
+                    .Select(err => err.ErrorMessage));
+
+                return BadRequest (errors);
             }
             return Content($"{person}");
         }
